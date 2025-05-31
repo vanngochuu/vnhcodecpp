ea818c0537d3049c8748271f053ceed0303916f8
vanngochuu <vanngochuu12@gmail.com>
2025-05-31 21:16:51 +0700

    DemTanSuat.cpp

--- DemTanSuat.cpp
+++ DemTanSuat.cpp
@@ -1,6 +1,7 @@
 /* Nhập một số nguyên dương n, tiếp theo là n số nguyên dương lần lượt là các phần tử của một dãy số, 
-   hãy đếm tần số (số lần xuất hiện) của các số trong dãy và in nó ra màn hình dưới dạng sau: "a1 - t1; a2 - t2; ... an - tn; ", 
-   trong đó t1 là số lần xuất hiện của số a1, t2 là số lần xuất hiện của a2, ... a1, a2, .. an không trùng nhau và được sắp xếp tăng dần (xem ví dụ để rõ hơn). */
+   hãy đếm tần số (số lần xuất hiện) của các số trong dãy và in nó ra màn hình dưới dạng sau: "a1 - t1; 
+   a2 - t2; ... an - tn; ", trong đó t1 là số lần xuất hiện của số a1, t2 là số lần xuất hiện của a2, ... 
+   a1, a2, .. an không trùng nhau và được sắp xếp tăng dần (xem ví dụ để rõ hơn).                        */
 #include<iostream>
 using namespace std;
 int main(){
