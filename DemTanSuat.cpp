1a2e1834b36c8f6dc146b662d727e27c76d12fa6
vanngochuu <vanngochuu12@gmail.com>
2025-05-31 17:02:44 +0700

    DemTanSuat.cpp

---
+++ DemTanSuat.cpp
@@ -0,0 +1,25 @@
+/* Nhập một số nguyên dương n, tiếp theo là n số nguyên dương lần lượt là các phần tử của một dãy số, 
+   hãy đếm tần số (số lần xuất hiện) của các số trong dãy và in nó ra màn hình dưới dạng sau: "a1 - t1; a2 - t2; ... an - tn; ", 
+   trong đó t1 là số lần xuất hiện của số a1, t2 là số lần xuất hiện của a2, ... a1, a2, .. an không trùng nhau và được sắp xếp tăng dần (xem ví dụ để rõ hơn). */
+#include<iostream>
+using namespace std;
+int main(){
+    int n, A[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> A[i];
+
+    int Max = A[0];
+    for (int i = 1; i < n; i++){
+         if (A[i] > Max) Max = A[i];
+    }
+
+    int B[100];
+    for (int i = 0; i <= Max; i++) B[i] = 0;
+
+    for (int i = 0; i < n; i++) B[A[i]] += 1;
+
+    for (int i = 0; i <= Max; i++){
+         if (B[i] > 0) cout << i << " - " << B[i] << "; ";
+    }
+    return 0;
+}
