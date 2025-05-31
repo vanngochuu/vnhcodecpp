c7e4b25cbd16c9964d3b6f065bc3d2cad104dd6e
vanngochuu <vanngochuu12@gmail.com>
2025-05-31 22:07:54 +0700

    GopMangABCodelearn.cpp

--- GopMangABCodelearn.cpp
+++ GopMangABCodelearn.cpp
@@ -1,1 +1,3 @@
-
+/* Cho dãy a gồm n số nguyên, và dãy b gồm m số nguyên, cả hai dãy đều được sắp xếp không giảm.
+   Hãy gộp hai dãy thành một dãy c, sao cho dãy c cũng là dãy không giảm. In dãy c ra màn hình, 
+   sau mỗi phần tử có đúng một dấy cách.                                                       */
