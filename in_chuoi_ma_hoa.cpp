ca3478a11bb73e1561ebbbe4c79ce31a9fb9a33b
vanngochuu <vanngochuu12@gmail.com>
2025-08-01 14:55:02 +0700

    CodeLearnDSA

--- in_chuoi_ma_hoa.cpp
+++ in_chuoi_ma_hoa.cpp
@@ -1,3 +1,7 @@
+/* Cho một sâu s chỉ gồm các ký tự viết thường, hãy viết hàm trả về chuỗi mã hóa của sâu này. 
+   Với s = "aaabbaaac" thì encodeString(s) = "a3b2a3c1".
+   Với s = "ab" thì encodeString(s) = "a1b1".
+   Với s = "aaddacc" thì encodeString(s) = "a2d2a1c2".                                       */                               
 #include<iostream>
 #include<stack>
 using namespace std;
